0b33dddcaa253b2a57008d529a168f24cd6450b6
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2023-12-07 13:47:08 +0100

    kata(mars-rover): add rover's command execution api

--- mars-rover/src/main/kotlin/com/abogomazov/marsrover/MarsRover.kt
+++ mars-rover/src/main/kotlin/com/abogomazov/marsrover/MarsRover.kt
@@ -3,7 +3,13 @@ package com.abogomazov.marsrover
 import com.abogomazov.marsrover.domain.Position
 
 class MarsRover(
-    private val position: Position,
-    private val commands: List<MovingCommand>
+    private var position: Position,
 ) {
+    fun execute(commands: List<MovingCommand>) {
+        this.position = commands.fold(position) { position, command ->
+            command.execute(position)
+        }
+    }
+
+    fun coordinate() = position.coordinate
 }
--- mars-rover/src/test/kotlin/com/abogomazov/marsrover/MarsRoverTest.kt
+++ mars-rover/src/test/kotlin/com/abogomazov/marsrover/MarsRoverTest.kt
@@ -1,11 +1,22 @@
 package com.abogomazov.marsrover
 
+import com.abogomazov.marsrover.domain.Coordinate
 import com.abogomazov.marsrover.domain.position
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 class MarsRoverTest {
     @Test
-    fun `coordinate, directory and list of commands are required to initialize rover`() {
-        MarsRover(position(), listOf(MoveForward))
+    fun `initial position is required to construct the rover`() {
+        MarsRover(position())
+    }
+
+    @Test
+    fun `execute updates rover position based on command list`() {
+        val sut = MarsRover(position())
+
+        sut.execute(listOf(MoveForward, MoveForward, TurnLeft, MoveBackward, TurnRight, MoveForward))
+
+        assertEquals(Coordinate(x = 1, y = 3), sut.coordinate())
     }
 }
