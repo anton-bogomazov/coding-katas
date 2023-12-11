00a02b7d4480900c452d116be0eec6b6b3d2966a
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2023-12-11 14:43:50 +0100

    kata(bowling): initialize game with 0 score

--- bowling/src/main/kotlin/com/abogomazov/bowling/Game.kt
+++ bowling/src/main/kotlin/com/abogomazov/bowling/Game.kt
@@ -2,5 +2,5 @@ package com.abogomazov.bowling
 
 class Game {
     fun roll(nKnockedPins: Int): Unit = TODO()
-    fun score(): Int = TODO()
+    fun score(): Int = 0
 }
--- bowling/src/test/kotlin/com/abogomazov/bowling/GameTest.kt
+++ bowling/src/test/kotlin/com/abogomazov/bowling/GameTest.kt
@@ -1,11 +1,12 @@
 package com.abogomazov.bowling
 
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 class GameTest {
     @Test
-    fun `sample`() {
-
+    fun `game initially has zero scores`() {
+        assertEquals(0, Game().score())
     }
 
 }
