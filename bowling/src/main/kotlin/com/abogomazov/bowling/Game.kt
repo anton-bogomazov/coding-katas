e4ead89b4411cedfae7932310b527676de213145
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2023-12-11 14:29:45 +0100

    kata(bowling): add initial Game class to follow requirements

---
+++ bowling/src/main/kotlin/com/abogomazov/bowling/Game.kt
@@ -0,0 +1,6 @@
+package com.abogomazov.bowling
+
+class Game {
+    fun roll(nKnockedPins: Int): Unit = TODO()
+    fun score(): Int = TODO()
+}
---
+++ bowling/src/test/kotlin/com/abogomazov/bowling/GameTest.kt
@@ -0,0 +1,11 @@
+package com.abogomazov.bowling
+
+import org.junit.jupiter.api.Test
+
+class GameTest {
+    @Test
+    fun `sample`() {
+
+    }
+
+}
